bede74aceb2f53675b9a64f4a58b3e287ddc407b
ugur-vaadin <ugur@vaadin.com>
2022-11-18 14:42:09 +0200

    test: add instructions to the files

--- frontend/views/date-picker-view/date-picker-view.ts
+++ frontend/views/date-picker-view/date-picker-view.ts
@@ -10,11 +10,20 @@ export class DatePickerView extends View {
    *
    * 1.
    * 1.1. Add a Date Picker to the view
-   * 1.2. ???
+   * 1.2. Set a valid custom reference date using i18n
    *
    * 2.
-   * 2.1. ???
-   * 2.2. ???
+   * 2.1. Add a Date Picker to the view
+   * 2.2. Set an invalid custom reference date using i18n
+   *
+   * 3.
+   * 3.1. Add a Date Picker to the view
+   * 3.2. Set a locale to the Date Picker
+   * 3.3. Add another Date Picker to the view
+   * 3.4. Set a short year custom date format to the Date Picker
+   * 3.5. Add another Date Picker to the view
+   * 3.6. Set a 4-digit year custom date format to the Date Picker
+   * 3.7. Add fields that display the value of each Date Picker
    *
    */
   render() {
--- src/main/java/com/example/application/views/datepickerview/DatePickerView.java
+++ src/main/java/com/example/application/views/datepickerview/DatePickerView.java
@@ -15,11 +15,20 @@ public class DatePickerView extends VerticalLayout {
      *
      * 1.
      * 1.1. Add a Date Picker to the view
-     * 1.2. ???
+     * 1.2. Set a valid custom reference date using i18n
      *
      * 2.
-     * 2.1. ???
-     * 2.2. ???
+     * 2.1. Add a Date Picker to the view
+     * 2.2. Set an invalid custom reference date using i18n
+     *
+     * 3.
+     * 3.1. Add a Date Picker to the view
+     * 3.2. Set a locale to the Date Picker
+     * 3.3. Add another Date Picker to the view
+     * 3.4. Set a short year custom date format to the Date Picker
+     * 3.5. Add another Date Picker to the view
+     * 3.6. Set a 4-digit year custom date format to the Date Picker
+     * 3.7. Add fields that display the value of each Date Picker
      *
      */
     public DatePickerView() {

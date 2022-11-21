c1b06bbdfb1d4c6dd828a767daf2a090a3588075
ugur-vaadin <ugur@vaadin.com>
2022-11-22 01:23:10 +0200

    test: update dx test steps

--- frontend/views/date-picker-view/date-picker-view.ts
+++ frontend/views/date-picker-view/date-picker-view.ts
@@ -9,21 +9,22 @@ export class DatePickerView extends View {
    * Tasks to complete:
    *
    * 1.
-   * 1.1. Add a Date Picker to the view
-   * 1.2. Set a valid custom reference date using i18n
+   * 1.1. Create a page where users can enter events that happened during World War 1.
+   * 1.2. Each event should have a name and a date.
+   * 1.3. The events should be displayed in a Grid.
+   * 1.4. Make sure that the entered dates with 2-digit years are interpreted as dates related
+   *      to World War 1 events (1914-1918).
    *
    * 2.
-   * 2.1. Add a Date Picker to the view
-   * 2.2. Set an invalid custom reference date using i18n
+   * 2.1. A 4-digits year presentation is superfluous in this context. As a follow-up,
+   *      modify the page in order to display the year values using 2 digits.
+   * 2.2. Modify the page in order to accept only dates with 2-digit years.
    *
    * 3.
-   * 3.1. Add a Date Picker to the view
-   * 3.2. Set a locale to the Date Picker
-   * 3.3. Add another Date Picker to the view
-   * 3.4. Set a short year custom date format to the Date Picker
-   * 3.5. Add another Date Picker to the view
-   * 3.6. Set a 4-digit year custom date format to the Date Picker
-   * 3.7. Add fields that display the value of each Date Picker
+   * 3.1. Try entering dates between 0-99 with and without zero padding in a date picker
+   *      with short year format
+   * 3.2. Try entering dates between 0-999 with and without zero padding in a date picker
+   *      with 4-digit year format or locale
    *
    */
   render() {

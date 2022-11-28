d6338afbbd3d5ea71d8878d867b075994d830fbd
ugur-vaadin <ugur@vaadin.com>
2022-11-28 10:57:35 +0200

    test: update tasks

--- src/main/java/com/example/application/views/datepickerview/DatePickerView.java
+++ src/main/java/com/example/application/views/datepickerview/DatePickerView.java
@@ -30,16 +30,7 @@ public class DatePickerView extends VerticalLayout {
      *      and a date. Make sure that the entered dates with 2-digit years are interpreted
      *      as dates related to World War 1 events (1914-1918).
      *
-     * 2.
-     * 2.1. A 4-digits year presentation is superfluous in this context. As a follow-up,
-     *      modify the page in order to display the year values using 2 digits.
-     * 2.2. Modify the page in order to accept only dates with 2-digit years.
-     *
-     * 3.
-     * 3.1. Try entering dates between 0-99 with and without zero padding in a date picker
-     *      with short year format
-     * 3.2. Try entering dates between 0-999 with and without zero padding in a date picker
-     *      with 4-digit year format or locale
+     * 2. Try entering a date with a year between 0-99.
      *
      */
 
